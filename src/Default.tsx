d1246c59a1c45ceea3a17e20683d4e566700337e
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-10-25 13:07:37 +0100

    receive dynamic configuration

--- src/Default.tsx
+++ src/Default.tsx
@@ -5,30 +5,76 @@ import SimpleLineChart from "./lib/SimpleLineChart";
 import StackedAreaChart from "./lib/StackedAreaChart";
 import StraightAnglePieChart from "./lib/StraightAnglePieChart";
 
+const props = [
+  {
+    type: "bar",
+    props: {
+      labels: ["pv", "uv"]
+    }
+  },
+  {
+    type: "line",
+    props: {
+      labels: ["pv", "amt"]
+    }
+  },
+  {
+    type: "area",
+    props: {
+      labels: ["amt", "uv"]
+    }
+  },
+  {
+    type: "pie",
+    props: {
+      labels: ["pv", "uv", "amt"]
+    }
+  },
+  {
+    type: "gauge",
+    props: {
+      label: "pv"
+    }
+  },
+  {
+    type: "gauge",
+    props: {
+      label: "uv"
+    }
+  },
+  {
+    type: "gauge",
+    props: {
+      label: "amt"
+    }
+  },
+];
+
+
+const TypeToComp = {
+  gauge: SimpleGauge,
+  bar: SimpleBarChart,
+  line: SimpleLineChart,
+  area: StackedAreaChart,
+  pie: StraightAnglePieChart
+};
+
+function renderComponent(compProps: any, key: number) {
+  const type = compProps.type;
+  // @ts-ignore
+  const Component = TypeToComp[type];
+  return (
+    <Grid size={6}>
+      <Component key={key} {...{...compProps.props}} />
+    </Grid>
+  )
+
+}
+
 export default function Dashboard() {
   return (
       <Grid container spacing={2}>
-        <Grid size={6}>
-          <SimpleBarChart labels={["pv", "uv"]}/>
-        </Grid>
-        <Grid size={6}>
-          <SimpleLineChart labels={["pv", "amt"]} />
-        </Grid>
-        <Grid size={6}>
-          <StackedAreaChart labels={["amt", "uv"]} />
-        </Grid>
-        <Grid size={6}>
-          <StraightAnglePieChart labels={["pv", "uv", "amt"]} />
-        </Grid>
-        <Grid size={6}>
-          <SimpleGauge label="pv"/>
-        </Grid>
-        <Grid size={6}>
-          <SimpleGauge label="uv"/>
-        </Grid>
-        <Grid size={6}>
-          <SimpleGauge label="amt"/>
-        </Grid>
+        {props.map((prop, i) => renderComponent(prop, i))}
       </Grid>
   )
 }
