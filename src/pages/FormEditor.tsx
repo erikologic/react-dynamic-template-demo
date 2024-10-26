5f4cc07dfd5e830dd6dc0bcf78784d2f89ee869a
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-10-26 11:29:44 +0100

    added rsjf

--- src/pages/FormEditor.tsx
+++ src/pages/FormEditor.tsx
@@ -1,7 +1,29 @@
 import { FunctionComponent } from "react";
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
 
-const FormEditor: FunctionComponent = () => {
-    return ( <div>Form editor</div> );
-}
+const schema: RJSFSchema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = (type: any) => console.log.bind(console, type);
+
+const FormEditor: FunctionComponent = () =>
+    (
+        <Form
+          schema={schema}
+          validator={validator}
+          onChange={log('changed')}
+          onSubmit={log('submitted')}
+          onError={log('errors')}
+        />
+      );
  
 export default FormEditor;
