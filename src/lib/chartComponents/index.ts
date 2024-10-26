cf49594ff6f7aed18bcbd068747cf74d468f1b2d
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-10-26 12:28:24 +0100

    one point of registration for chart components

---
+++ src/lib/chartComponents/index.ts
@@ -0,0 +1,13 @@
+import * as SimpleLineChart from './SimpleLineChart'
+import * as SimpleGauge from './SimpleGauge'
+import * as SimpleBarChart from './SimpleBarChart'
+import * as StackedAreaChart from './StackedAreaChart'
+import * as StraightAnglePieChart from './StraightAnglePieChart'
+
+export const charts = [
+    SimpleLineChart,
+    SimpleGauge,
+    SimpleBarChart,
+    StackedAreaChart,
+    StraightAnglePieChart
+] as const;
